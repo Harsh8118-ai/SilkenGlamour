a2c08064a714f928dd6beef652ea022a074fd79a
Harsh8118-ai <tyagih7654321@gmail.com>
2024-09-15 17:58:13 +0530

    Final Code

--- Frontend/src/components/Contact/SignUp.jsx
+++ Frontend/src/components/Contact/SignUp.jsx
@@ -30,7 +30,7 @@ const SignUp = () => {
 
 
 
-        try {const response = await fetch('https://silken-glamour.vercel.app/api/auth/register', {
+        try {const response = await fetch('https://silken-glamour.vercel.app/register', {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
