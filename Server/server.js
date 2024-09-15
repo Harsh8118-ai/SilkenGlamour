3b83469bee55ca82e200ec6ea17a24f8310d5567
Harsh Tyagi <tyagih7654321@gmail.com>
2024-09-15 16:39:32 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -9,7 +9,7 @@ const connectDb = require("./utils/db");
 const errorMiddleware = require("./middlewares/error-middleware");
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://silken-glamour-frontend.vercel.app/",
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
   credentials: true,
 };
