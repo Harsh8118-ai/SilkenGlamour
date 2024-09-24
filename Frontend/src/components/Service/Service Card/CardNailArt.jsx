41787961b61626b045681f47c53b9d070664facc
Harsh8118-ai <tyagih7654321@gmail.com>
2024-09-24 17:01:45 +0530

    Final Code

--- Frontend/src/components/Service/Service Card/CardNailArt.jsx
+++ Frontend/src/components/Service/Service Card/CardNailArt.jsx
@@ -112,152 +112,154 @@ const CardNailArt = () => {
     //     </div>      
     <>
 
-<div className="flex justify-between w-full h-full">
-  {/* Left Sticky Container */}
-  <div className="hidden sm:block w-1/4 sticky top-20 h-screen bg-gray-100 p-4">
-  <div className='bg-white'>
+      <div className="flex justify-between w-full h-full">
+        {/* Left Sticky Container */}
+        <div className="hidden sm:block w-1/4 sticky top-20 h-screen bg-gray-100 p-4">
+          <div className='bg-white'>
             <h1 className={`text-center text-3xl font-bold p-3 ${LogoColor}`}>Services</h1>
             <div className='flex justify-center items-center p-5 '>
-                <div className="flex flex-col gap-2">
-                    
-                    <Link to="nailart">
-                    <div className='shadow-lg rounded-xl p-2 flex flex-col bg-gray-900 justify-center items-center hover:scale-125 '>
-                    
+              <div className="flex flex-col gap-2">
+
+                <Link to="nailart">
+                  <div className='shadow-lg rounded-xl p-2 flex flex-col bg-gray-900 justify-center items-center hover:scale-125 '>
+
                     <p className={`font-bold pt-1.5 ${LogoColor}`}>Nail Art</p>
-                    </div></Link>
-                    
-                    <Link to="haircare">
-                    <div className='shadow-lg rounded-xl p-2 flex flex-col bg-gray-900 justify-center items-center hover:scale-125'>
-                    
+                  </div></Link>
+
+                <Link to="haircare">
+                  <div className='shadow-lg rounded-xl p-2 flex flex-col bg-gray-900 justify-center items-center hover:scale-125'>
+
                     <p className={`font-bold pt-1.5 ${LogoColor}`}>Hair Care</p>
-                    </div></Link>
+                  </div></Link>
 
-                    <Link to="mani-pedi">
-                    <div className='shadow-lg rounded-xl p-2 flex flex-col bg-gray-900 justify-center items-center hover:scale-125'>
-                    
+                <Link to="mani-pedi">
+                  <div className='shadow-lg rounded-xl p-2 flex flex-col bg-gray-900 justify-center items-center hover:scale-125'>
+
                     <p className={`font-bold pt-1.5 ${LogoColor}`}>Mani-Pedi</p>
-                    </div></Link>
+                  </div></Link>
 
-                    <Link to="waxing">
-                    <div className='shadow-lg rounded-xl p-2 flex flex-col bg-gray-900 justify-center items-center hover:scale-125'>
-                    
+                <Link to="waxing">
+                  <div className='shadow-lg rounded-xl p-2 flex flex-col bg-gray-900 justify-center items-center hover:scale-125'>
+
                     <p className={`font-bold pt-1.5 ${LogoColor}`}>Waxing</p>
-                    </div></Link>
+                  </div></Link>
 
-                    <Link to="body-polishing">
-                    <div className='shadow-lg rounded-xl p-2 flex flex-col bg-gray-900 justify-center items-center hover:scale-125'>
-                    
+                <Link to="body-polishing">
+                  <div className='shadow-lg rounded-xl p-2 flex flex-col bg-gray-900 justify-center items-center hover:scale-125'>
+
                     <p className={`font-bold pt-1.5 ${LogoColor}`}>Polish & Massage</p>
-                    </div></Link>
+                  </div></Link>
 
-                    <Link to="threading">
-                    <div className='shadow-lg rounded-xl p-2 flex flex-col bg-gray-900 justify-center items-center hover:scale-125'>
-                    
+                <Link to="threading">
+                  <div className='shadow-lg rounded-xl p-2 flex flex-col bg-gray-900 justify-center items-center hover:scale-125'>
+
                     <p className={`font-bold pt-1.5 ${LogoColor}`}>Threading & Wax</p>
-                     </div></Link>
+                  </div></Link>
 
-                    <Link to="bleach-dtan">
-                    <div className='shadow-lg rounded-xl p-2 flex flex-col bg-gray-900 justify-center items-center hover:scale-125'>
-                    
+                <Link to="bleach-dtan">
+                  <div className='shadow-lg rounded-xl p-2 flex flex-col bg-gray-900 justify-center items-center hover:scale-125'>
+
                     <p className={`font-bold pt-1.5 ${LogoColor}`}>Bleach & D-Tan</p>
-                    </div></Link>
+                  </div></Link>
 
-                    <Link to="weddingpakage">
-                    <div className='shadow-lg rounded-xl p-2 flex flex-col bg-gray-900 justify-center items-center hover:scale-125'>
-                    
+                <Link to="weddingpakage">
+                  <div className='shadow-lg rounded-xl p-2 flex flex-col bg-gray-900 justify-center items-center hover:scale-125'>
+
                     <p className={`font-bold pt-1.5 ${LogoColor}`}>Wedding Package</p>
-                    </div></Link>
+                  </div></Link>
 
-                    
 
-                    <Link to="facial-cleanup">
-                    <div className='shadow-lg rounded-xl p-2 sm:hidden text-center
+
+                <Link to="facial-cleanup">
+                  <div className='shadow-lg rounded-xl p-2 sm:hidden text-center
                     lg:flex lg:justify-center lg:items-center lg:flex-col bg-gray-900 hover:scale-125'>
-                    
+
                     <p className={`font-bold pt-1.5 ${LogoColor}`}>Facial & Cleanup</p>
-                    </div></Link>
-                    <div className='m-4'></div>
+                  </div></Link>
+                <div className='m-4'></div>
 
-        </div >
-        </div >
+              </div >
+            </div >
+          </div>
+          {/* Add content for the left container */}
         </div>
-    {/* Add content for the left container */}
-  </div>
 
-  {/* Middle Container (Products Container - 45% Width) */}
-  <div className="w-[80%] sm:w-[45%] mx-auto my-8">
-    <div className="w-full mx-auto">
-      <h1 className="text-2xl font-bold mb-4 text-center">Select your Item :</h1>
-      <div className="space-x-4 text-center">
-        <button
-          className={`px-4 py-2 ${IsSelect ? "bg-yellow-500" : "bg-gray-400"} ${IsSelect ? "border-2" : "border-none"} border-yellow-700 text-white rounded`}
-          onClick={() => {
-            setType('NailExtension');
-            toggleActive();
-            toggleSelect1();
-          }}
-        >
-          Nail Extension
-        </button>
-        <button
-          className={`px-4 py-2 ${IsSelect2 ? "bg-yellow-500" : "bg-gray-400"} ${IsSelect2 ? "border-2" : "border-none"} border-yellow-700 text-white rounded`}
-          onClick={() => {
-            setType('NailArtWork');
-            toggleActive();
-            toggleSelect2();
-          }}
-        >
-          Art Work
-        </button>
-      </div>
-
-      {products.length > 0 && (
-        <div className="w-full h-full mt-6">
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-4 gap-y-8 ">
-            {products.map((product) => (
-              <div key={product.id} className="shadow border-gray-700 rounded-lg bg-gray-200 sm:p-4 p-2">
-                <a href={product.href}>
-                  <img className="rounded-lg h-fit w-fit object-cover" src={product.image} alt={product.image} />
-                </a>
-                <div className="px-5 pb-5">
-                  <a href={product.href}>
-                    <h5 className={`text-xl tracking-tight text-center mb-3 font-bold ${GoldenColor}`}>
-                      {product.name}
-                    </h5>
-                  </a>
-                  <div className="flex items-center justify-between">
-                    <span className={`text-2xl font-bold ${PriceColor}`}>₹{product.price}</span>
-                    <span className="text-red-500 m-2 mx-1 text-sm font-bold line-through">₹{product.offerprice}</span>
-                    <a
-                      href={product.href}
-                      className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-1 sm:px-5 py-2.5 text-center"
-                    >
-                      Add to cart
-                    </a>
-                  </div>
-                  {product.features.map((feature, index) => (
-                    <div key={index} className="flex gap-2 w-full mt-2">
-                      <img src="/Services/Tick.svg" alt="tick" />
-                      <span className="text-xs sm:text-sm text-black">{feature}</span>
-                    </div>
-                  ))}
-                </div>
+        {/* Middle Container (Products Container - 45% Width) */}
+        <div className="w-[80%] sm:w-[45%] mx-auto my-8">
+          <div className={`w-fit max-w-xl h-fit m-3 mx-auto`}>
+            <div className="w-full mx-auto sticky top-6 flex flex-col justify-center items-center h-fit">
+              <h1 className="text-2xl font-bold mb-4 text-center">Select your Item :</h1>
+              <div className="space-x-4 text-center">
+                <button
+                  className={`px-4 py-2 ${IsSelect ? "bg-yellow-500" : "bg-gray-400"} ${IsSelect ? "border-2" : "border-none"} border-yellow-700 text-white rounded`}
+                  onClick={() => {
+                    setType('NailExtension');
+                    toggleActive();
+                    toggleSelect1();
+                  }}
+                >
+                  Nail Extension
+                </button>
+                <button
+                  className={`px-4 py-2 ${IsSelect2 ? "bg-yellow-500" : "bg-gray-400"} ${IsSelect2 ? "border-2" : "border-none"} border-yellow-700 text-white rounded`}
+                  onClick={() => {
+                    setType('NailArtWork');
+                    toggleActive();
+                    toggleSelect2();
+                  }}
+                >
+                  Art Work
+                </button>
               </div>
-            ))}
+
+              {products.length > 0 && (
+                <div className="w-full h-full mt-6">
+                  <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-4 gap-y-8 ">
+                    {products.map((product) => (
+                      <div key={product.id} className="shadow border-gray-700 rounded-lg bg-gray-200 sm:p-4 p-2">
+                        <a href={product.href}>
+                          <img className="rounded-lg h-fit w-fit object-cover" src={product.image} alt={product.image} />
+                        </a>
+                        <div className="px-5 pb-5">
+                          <a href={product.href}>
+                            <h5 className={`text-xl tracking-tight text-center mb-3 font-bold ${GoldenColor}`}>
+                              {product.name}
+                            </h5>
+                          </a>
+                          <div className="flex items-center justify-between">
+                            <span className={`text-2xl font-bold ${PriceColor}`}>₹{product.price}</span>
+                            <span className="text-red-500 m-2 mx-1 text-sm font-bold line-through">₹{product.offerprice}</span>
+                            <a
+                              href={product.href}
+                              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-1 sm:px-5 py-2.5 text-center"
+                            >
+                              Add to cart
+                            </a>
+                          </div>
+                          {product.features.map((feature, index) => (
+                            <div key={index} className="flex gap-2 w-full mt-2">
+                              <img src="/Services/Tick.svg" alt="tick" />
+                              <span className="text-xs sm:text-sm text-black">{feature}</span>
+                            </div>
+                          ))}
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+              )}
+            </div>
           </div>
         </div>
-      )}
-    </div>
-  </div>
-
-  {/* Right Sticky Container */}
-  <div className="hidden sm:block w-1/4 sticky top-0 h-screen bg-gray-100 p-4">
-    <h2 className="text-center text-xl font-bold mb-4">Right Container</h2>
-    {/* Add content for the right container */}
-  </div>
 
 
-  </div>
+          {/* Right Sticky Container */}
+          <div className="hidden sm:block w-1/4 sticky top-0 h-screen bg-gray-100 p-4">
+            <h2 className="text-center text-xl font-bold mb-4">Right Container</h2>
+            {/* Add content for the right container */}
+          </div>
+
+      </div>
 
     </>
   );
