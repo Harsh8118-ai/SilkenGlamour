cef1e823c535115521d6783cfc2ce2d7cbeef354
Harsh Tyagi <tyagih7654321@gmail.com>
2024-10-22 09:27:41 +0530

    Final Code

---
+++ Frontend/src/components/Profile/ConfirmationModalAddress.jsx
@@ -0,0 +1,188 @@
+import React, { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { useAuth } from '../../Store/auth';
+import { ToastContainer, toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
+import { FaTimes } from 'react-icons/fa';
+
+
+const ConfirmationModalAddress = ({ data, isOpen, onClose }) => {
+    // console.log("data",data);
+
+    const [token, setToken] = useState(localStorage.getItem("token"));
+    const { user } = useAuth();
+
+    const navigate = useNavigate();
+
+    // Access environment variable using import.meta.env
+    const BASE_URL = import.meta.env.VITE_BACKEND_URL;
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+        setValue,
+    } = useForm();
+
+    useEffect(() => {
+        if (user) {
+            setValue('street', data?.street);
+            setValue('apartmentNumber', data?.apartmentNumber);
+            setValue('town', data?.town);
+            setValue('pincode', data?.pincode);
+        }
+    }, [user, setValue]);
+
+    const submitProfileForm = async (formData) => {
+        try {
+            // Form submission logic here
+            console.log("Dataa", formData);
+            const response = await fetch(`${BASE_URL}/auth/updateProfile`, {
+                // const response = await fetch('http://localhost:5000/api/auth/register', {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${token}`, // Indicating that JSON is being sent
+                },
+                body: JSON.stringify(formData), // Converting formData object to JSON string
+
+
+            });
+
+
+            console.log("Updation Response", response);
+            const data = await response.json(); // Parsing the response data
+
+
+
+
+            if (response.ok) {
+                toast.success("Update Successfully");
+                console.log('User Updated successfully:', data); // Success message
+
+                navigate("/profile");
+
+                setTimeout(() => {
+                    window.location.reload();
+                }, 500);
+            }
+            else {
+
+
+                toast(data.message);
+
+
+                console.log("Response", data);
+
+
+
+            }
+
+        } catch (error) {
+            // Handle error
+            console.log("error occur while updation", error.message);
+            console.log(error);
+
+
+        }
+    };
+
+    // If modal is not open, return null (won't render)
+    if (!isOpen) return null;
+
+    return (
+        <>
+            <div className="fixed inset-0 bg-black bg-opacity-50 z-50 flex justify-center items-center">
+                <div className="bg-MainBGColorYellow p-6 rounded-lg shadow-lg max-w-lg w-full relative mx-4 sm:mx-0">
+                    <button
+                        onClick={onClose}
+                        className="absolute top-2 right-2 text-red-500 hover:text-red-800 text-bold"
+                    >
+                        <FaTimes className="h-6 w-6" />
+                    </button>
+                    <h2 className="text-2xl text-BGColorYellow font-bold mb-6 text-center">Your Address</h2>
+                    <form onSubmit={handleSubmit(submitProfileForm)}>
+                        <div className="space-y-4">
+                            {/* Address Information */}
+                            <div className="space-y-4">
+                                {/* Street */}
+                                <div>
+                                    <label className="block text-BGColorYellow text-sm font-bold" htmlFor="street">
+                                        Street
+                                    </label>
+                                    <input
+                                        type="text"
+                                        id="street"
+                                        className="w-full px-3 py-2 text-gray-300 border border-gray-300 bg-gray-700 rounded-md focus:outline-none focus:border-green-500"
+                                        placeholder="Enter Street"
+                                        {...register('street', { required: true })}
+                                    />
+                                    {errors.street && <span className="text-red-500 text-sm">Please enter your Street</span>}
+                                </div>
+
+                                {/* Apartment Number */}
+                                <div>
+                                    <label className="block text-BGColorYellow text-sm font-bold" htmlFor="apartmentNumber">
+                                        Apartment Number
+                                    </label>
+                                    <input
+                                        type="text"
+                                        id="apartmentNumber"
+                                        className="w-full px-3 py-2 text-gray-300 border border-gray-300 bg-gray-700 rounded-md focus:outline-none focus:border-green-500"
+                                        placeholder="Enter Apartment Number"
+                                        {...register('apartmentNumber', { required: true })}
+                                    />
+                                    {errors.apartmentNumber && <span className="text-red-500 text-sm">Please enter your Apartment Number</span>}
+                                </div>
+
+                                {/* Town */}
+                                <div>
+                                    <label className="block text-BGColorYellow text-sm font-bold" htmlFor="town">
+                                        Town
+                                    </label>
+                                    <input
+                                        type="text"
+                                        id="town"
+                                        className="w-full px-3 py-2 text-gray-300 border border-gray-300 bg-gray-700 rounded-md focus:outline-none focus:border-green-500"
+                                        placeholder="Enter Town"
+                                        {...register('town', { required: true })}
+                                    />
+                                    {errors.town && <span className="text-red-500 text-sm">Please enter your Town</span>}
+                                </div>
+
+                                {/* Pincode */}
+                                <div>
+                                    <label className="block text-BGColorYellow text-sm font-bold" htmlFor="pincode">
+                                        Pincode
+                                    </label>
+                                    <input
+                                        type="text"
+                                        id="pincode"
+                                        className="w-full px-3 py-2 text-gray-300 border border-gray-300 bg-gray-700 rounded-md focus:outline-none focus:border-green-500"
+                                        placeholder="Enter Pincode"
+                                        {...register('pincode', { required: true })}
+                                    />
+                                    {errors.pincode && <span className="text-red-500 text-sm">Please enter your Pincode</span>}
+                                </div>
+                            </div>
+
+                        </div>
+
+                        {/* Submit Button */}
+                        <div className="mt-6 flex justify-center">
+                            <button
+                                type="submit"
+                                className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-md transition duration-200 ease-in-out"
+                            >
+                                Submit
+                            </button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+        </>
+    );
+};
+
+export default ConfirmationModalAddress;
--- Frontend/src/components/Profile/Profile.jsx
+++ Frontend/src/components/Profile/Profile.jsx
@@ -3,15 +3,18 @@ import { Link } from 'react-router-dom';
 import { useAuth } from '../../Store/auth';
 import Login from '../Contact/LogIn';
 import ConfirmationModalName from './ConfirmationModalName';
+import ConfirmationModalAddress from './ConfirmationModalAddress';
 
 const Profile = () => {
   const { user } = useAuth();
   const { isLoggedIn } = useAuth();
-  const [ModalData, setModalData] = useState(null);
+  const [modalType, setModalType] = useState(null); // State to determine which modal to open
+  const [modalData, setModalData] = useState(null); // State to store modal data
   const [isModalOpen, setIsModalOpen] = useState(false); // State to open/close modal
 
-  // Function to open the modal with data
-  const openModal = (data) => {
+  // Function to open the modal with data and type
+  const openModal = (type, data) => {
+    setModalType(type);
     setModalData(data);
     setIsModalOpen(true);
   };
@@ -19,155 +22,143 @@ const Profile = () => {
   // Function to close the modal
   const closeModal = () => {
     setIsModalOpen(false);
+    setModalType(null);
+    setModalData(null);
   };
-
 
   return (
     <>
-    {isLoggedIn ?
-      <div className='h-full w-screen bg-BGColorYellow p-5'>
-        <div className="max-w-7xl mx-auto py-10 px-4 bg-gradient-to-b from-[#CBB59F] to-[#DED6CB] min-h-screen rounded-xl">
-          {/* Heading */}
-          <h1 className="text-4xl font-bold font-Helvetica mb-6 text-[#7A6752]">Your Profile</h1>
+      {isLoggedIn ? (
+        <div className='h-full w-screen bg-BGColorYellow p-5'>
+          <div className="max-w-7xl mx-auto py-10 px-4 bg-gradient-to-b from-[#CBB59F] to-[#DED6CB] min-h-screen rounded-xl">
+            {/* Heading */}
+            <h1 className="text-4xl font-bold font-Helvetica mb-6 text-[#7A6752]">Your Profile</h1>
 
-          {/* Profile Info Section WEB View */}
-          <div className="bg-[#E3DBD0] hidden sm:block shadow-2xl rounded-lg p-6 mb-10 transition-all duration-500 ease-in-out transform hover:scale-105">
-            <h2 className="text-2xl font-medium mb-4 text-[#7A6752]">Personal Information</h2>
-            <div className="flex items-center space-x-6">
-              
-              <div>
-                <h3 className="text-xl font-semibold text-[#7A6752]">{user.username}</h3>
-                
-                <p className="text-lg text-black">Email: {user.email}</p>
-                <p className="text-lg text-black">Phone: {user.mobileNumber}</p>
+            {/* Profile Info Section WEB View */}
+            <div className="bg-[#E3DBD0] hidden sm:block shadow-2xl rounded-lg p-6 mb-10 transition-all duration-500 ease-in-out transform hover:scale-105">
+              <h2 className="text-2xl font-medium mb-4 text-[#7A6752]">Personal Information</h2>
+              <div className="flex items-center space-x-6">
+                <div>
+                  <h3 className="text-xl font-semibold text-[#7A6752]">{user.username}</h3>
+                  <p className="text-lg text-black">Email: {user.email}</p>
+                  <p className="text-lg text-black">Phone: {user.mobileNumber}</p>
+                </div>
+
+                {/* Log Out Button WEB VIEW */}
+                <div className="items-center sm:order-2 hidden sm:flex absolute flex-col gap-4 right-10">
+                  <Link
+                    to="/contact/logout"
+                    className="text-MainBGColorYellow hover:bg-MainBGColorYellow focus:ring-4 bg-gray-900 focus:ring-gray-600 font-medium rounded-lg text-sm px-4 sm:px-3 py-2 sm:py-1.5 mr-2 lg:mr-4 focus:outline-none hover:text-black"
+                  >
+                    Log Out
+                  </Link>
+                </div>
               </div>
 
-              {/* .............. LOG IN / LOG OUT .............. */}
-              {/* WEB VIEW  */}
-              <div className="items-center sm:order-2 hidden sm:flex absolute flex-col gap-4 right-10">
-                 <Link
-                  to="/contact/logout"
-                  className="text-MainBGColorYellow hover:bg-MainBGColorYellow focus:ring-4 bg-gray-900 focus:ring-gray-600 font-medium rounded-lg text-sm px-4 sm:px-3 py-2 sm:py-1.5 mr-2 lg:mr-4 focus:outline-none hover:text-black"
-                >
-                  Log Out
-                  
-
-                </Link>
-              </div>
-
-            </div>
-            {/* Edit Profile Button */}
+              {/* Edit Profile Button */}
               <div className="mt-4">
                 <button
                   className="text-[#796855] hover:text-[#CBB59F] font-medium transition-all duration-300"
-                  onClick={() =>
-                    openModal({
-                      username: user?.username,
-                      // email: user.email,
-                      mobileNumber: user.mobileNumber,
-                    })
-                  }
+                  onClick={() => openModal('name', { username: user?.username })}
                 >
                   Edit Profile
                 </button>
               </div>
-          </div>
-
-          {/* Profile Info Section Mobile View */}
-          <div className="bg-[#E3DBD0] sm:hidden shadow-2xl rounded-lg p-6 mb-10 transition-all duration-500 ease-in-out transform hover:scale-105">
-            <h2 className="text-2xl font-medium mb-4 text-[#7A6752]">Personal Information</h2>
-            <div className="flex items-center flex-col justify-center ">
-              <div>
-                <h3 className="text-xl text-center font-semibold text-[#7A6752]">{user.username}</h3>
-                <p className="text-lg text-black">Phone: {user.mobileNumber}</p>
-                <p className="text-lg text-black">Email: {user.email}</p>
-              </div>
-              <div className="my-2">
-              <button className="text-[#796855] hover:text-[#CBB59F] font-medium transition-all duration-300" onClick={()=> {
-                SetModal({
-                  username: user?.username,
-                  // email: user.email,
-                  // mobileNumber: user.mobileNumber,
-                })
-              }}>
-                Edit Profile
-              
-              </button>
-               
             </div>
 
-              {/* .............. LOG IN / LOG OUT .............. */}
-              {/* Mobile VIEW  */}
-              <div className="items-center flex gap-2 justify-center mt-2">
-                <Link
-                  to="/contact/logout"
-                  className="text-MainBGColorYellow hover:bg-MainBGColorYellow focus:ring-4 bg-gray-900 focus:ring-gray-600 font-medium rounded-lg text-sm px-4 sm:px-3 py-2 sm:py-1.5 mr-2 lg:mr-4 focus:outline-none hover:text-black">
-                    Log Out</Link>
-              </div>
-            </div>
-          </div>
-
-          {/* Address Management */}
-          <div className="bg-[#E3DBD0] shadow-2xl rounded-lg p-6 transition-all duration-500 ease-in-out transform hover:scale-105 mb-10">
-            <h2 className="text-2xl font-medium mb-4 text-[#7A6752]">Your Address</h2>
-            <div className="space-y-4">
-              <div className="p-4 bg-[#DED6CB] rounded-lg">
-                <h3 className="text-xl font-semibold text-[#7A6752]">Home Address</h3>
-                <p className="text-lg">{user.street}, {user.apartmentNumber}</p>
-                <p className="text-lg">City: {user.town}</p>
-                <p className="text-lg">Zip Code: {user.pincode}</p>
-                <Link
-                  to="/edit-address"
-                  className="text-[#796855] hover:text-[#CBB59F] font-medium mt-2 block transition-all duration-300"
-                >
-                  Edit Address
-                </Link>
-              </div>
-            </div>
-          </div>
-
-          {/* Booking History */}
-          <div className="bg-[#E3DBD0] shadow-2xl rounded-lg p-6 transition-all duration-500 ease-in-out transform hover:scale-105">
-            <h2 className="text-2xl font-medium mb-4 text-[#7A6752]">Booking History</h2>
-            <div className="space-y-4">
-              <div className="p-4 bg-[#DED6CB] rounded-lg flex justify-between flex-col sm:flex-row gap-3 items-center">
+            {/* Profile Info Section Mobile View */}
+            <div className="bg-[#E3DBD0] sm:hidden shadow-2xl rounded-lg p-6 mb-10 transition-all duration-500 ease-in-out transform hover:scale-105">
+              <h2 className="text-2xl font-medium mb-4 text-[#7A6752]">Personal Information</h2>
+              <div className="flex items-center flex-col justify-center ">
                 <div>
-                  <h3 className="text-xl font-semibold text-[#7A6752]">Manicure & Pedicure</h3>
-                  <p className="text-lg">Date: Sept 20, 2024</p>
-                  <p className="text-lg">Status: Completed</p>
+                  <h3 className="text-xl text-center font-semibold text-[#7A6752]">{user.username}</h3>
+                  <p className="text-lg text-black">Phone: {user.mobileNumber}</p>
+                  <p className="text-lg text-black">Email: {user.email}</p>
                 </div>
-                <button className="bg-gradient-to-r from-[#2E2117] via-[#796855] to-[#2E2117] text-white px-4 py-2 rounded-md hover:bg-[#5E5543] transition transform hover:scale-105 duration-500">
-                  Rebook
-                </button>
-              </div>
+                <div className="my-2">
+                  <button
+                    className="text-[#796855] hover:text-[#CBB59F] font-medium transition-all duration-300"
+                    onClick={() => openModal('name', { username: user?.username })}
+                  >
+                    Edit Profile
+                  </button>
+                </div>
 
-              <div className="p-4 bg-[#DED6CB] rounded-lg flex justify-between flex-col sm:flex-row gap-3 items-center">
-                <div>
-                  <h3 className="text-xl font-semibold text-[#7A6752]">Facial & Clean-up</h3>
-                  <p className="text-lg">Date: Sept 12, 2024</p>
-                  <p className="text-lg">Status: Completed</p>
+                {/* Log Out Button Mobile VIEW */}
+                <div className="items-center flex gap-2 justify-center mt-2">
+                  <Link
+                    to="/contact/logout"
+                    className="text-MainBGColorYellow hover:bg-MainBGColorYellow focus:ring-4 bg-gray-900 focus:ring-gray-600 font-medium rounded-lg text-sm px-4 sm:px-3 py-2 sm:py-1.5 mr-2 lg:mr-4 focus:outline-none hover:text-black">
+                    Log Out
+                  </Link>
                 </div>
-                <button className="bg-gradient-to-r from-[#2E2117] via-[#796855] to-[#2E2117] text-white px-4 py-2 rounded-md hover:bg-[#5E5543] transition transform hover:scale-105 duration-500">
-                  Rebook
-                </button>
+              </div>
+            </div>
+
+            {/* Address Management */}
+            <div className="bg-[#E3DBD0] shadow-2xl rounded-lg p-6 transition-all duration-500 ease-in-out transform hover:scale-105 mb-10">
+              <h2 className="text-2xl font-medium mb-4 text-[#7A6752]">Your Address</h2>
+              <div className="space-y-4">
+                <div className="p-4 bg-[#DED6CB] rounded-lg">
+                  <h3 className="text-xl font-semibold text-[#7A6752]">Home Address</h3>
+                  <p className="text-lg">{user.street}, {user.apartmentNumber}</p>
+                  <p className="text-lg">City: {user.town}</p>
+                  <p className="text-lg">Zip Code: {user.pincode}</p>
+                  <button
+                    className="text-[#796855] hover:text-[#CBB59F] font-medium mt-2 block transition-all duration-300"
+                    onClick={() => openModal('address', {
+                      street: user?.street,
+                      apartmentNumber: user.apartmentNumber,
+                      town: user.town,
+                      pincode: user.pincode,
+                    })}
+                  >
+                    Edit Address
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            {/* Booking History */}
+            <div className="bg-[#E3DBD0] shadow-2xl rounded-lg p-6 transition-all duration-500 ease-in-out transform hover:scale-105">
+              <h2 className="text-2xl font-medium mb-4 text-[#7A6752]">Booking History</h2>
+              <div className="space-y-4">
+                <div className="p-4 bg-[#DED6CB] rounded-lg flex justify-between flex-col sm:flex-row gap-3 items-center">
+                  <div>
+                    <h3 className="text-xl font-semibold text-[#7A6752]">Manicure & Pedicure</h3>
+                    <p className="text-lg">Date: Sept 20, 2024</p>
+                    <p className="text-lg">Status: Completed</p>
+                  </div>
+                  <button className="bg-gradient-to-r from-[#2E2117] via-[#796855] to-[#2E2117] text-white px-4 py-2 rounded-md hover:bg-[#5E5543] transition transform hover:scale-105 duration-500">
+                    Rebook
+                  </button>
+                </div>
+
+                <div className="p-4 bg-[#DED6CB] rounded-lg flex justify-between flex-col sm:flex-row gap-3 items-center">
+                  <div>
+                    <h3 className="text-xl font-semibold text-[#7A6752]">Facial & Clean-up</h3>
+                    <p className="text-lg">Date: Sept 12, 2024</p>
+                    <p className="text-lg">Status: Completed</p>
+                  </div>
+                  <button className="bg-gradient-to-r from-[#2E2117] via-[#796855] to-[#2E2117] text-white px-4 py-2 rounded-md hover:bg-[#5E5543] transition transform hover:scale-105 duration-500">
+                    Rebook
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-
-          
         </div>
-        </div>
-          : <Login /> 
-          
-          
-            
-        }
-
-         {/* Render the ConfirmationModalName and pass necessary props */}
-      {ModalData && (
-        <ConfirmationModalName data={ModalData} isOpen={isModalOpen} onClose={closeModal} />
+      ) : (
+        <Login />
       )}
 
+      {/* Render the ConfirmationModals */}
+      {isModalOpen && modalType === 'name' && (
+        <ConfirmationModalName data={modalData} isOpen={isModalOpen} onClose={closeModal} />
+      )}
+      {isModalOpen && modalType === 'address' && (
+        <ConfirmationModalAddress data={modalData} isOpen={isModalOpen} onClose={closeModal} />
+      )}
     </>
   );
 };
